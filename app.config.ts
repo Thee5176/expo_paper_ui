18651c4aed675e1b7b09a21b47861a335228d1bf
Theerapong Likhitsab <Theerapong5176@gmail.com>
2026-01-29 08:42:24 +0900

    add theme and reconfig env loading

--- app.config.ts
+++ app.config.ts
@@ -1,16 +1,18 @@
-import * as dotenv from 'dotenv';
-import { ConfigContext, ExpoConfig } from 'expo/config';
+import * as dotenv from "dotenv";
+import { ConfigContext, ExpoConfig } from "expo/config";
 
 dotenv.config();
 
 export default ({ config }: ConfigContext): ExpoConfig => {
-    console.log(config.name);
-    return {
+  console.log(config.name);
+  return {
     ...config,
+    name: config.name ?? "native_expo_paper",
+    slug: config.slug ?? "native_expo_paper",
     plugins: [
       ...((config.plugins as any[]) || []),
       [
-        'react-native-auth0',
+        "react-native-auth0",
         {
           domain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
           customScheme: process.env.EXPO_PUBLIC_AUTH0_CUSTOM_SCHEME,
